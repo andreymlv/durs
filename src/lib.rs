2fa8dd12b3d09e105f2a7fa775cae4145e57f96d
Малов Андрей Владимирович <malov.av@barl.ru>
2025-01-06 00:01:33 +0300

    add docs for ls, ls_rec and size

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,28 @@
 use std::path::Path;
 use std::{fs::DirEntry, path::PathBuf};
 
+/// Lists the contents of the specified path.
+///
+/// If the path is a directory, this function returns a vector of `PathBuf` objects representing
+/// the files and directories within the directory.
+/// If the path is a file, this function returns a vector containing the path of the file.
+///
+/// # Examples
+///
+/// ```
+/// use durs::ls;
+/// use std::path::Path;
+///
+/// let files = ls(Path::new("/path/to/directory"))?;
+/// for file in files {
+///     println!("{}", file.display());
+/// }
+/// ```
+///
+/// # Errors
+///
+/// This function will return an error if the path cannot be accessed or if there is an error
+/// reading the directory contents.
 pub fn ls<P: AsRef<Path>>(path: P) -> anyhow::Result<Vec<PathBuf>> {
     let meta = path.as_ref().symlink_metadata()?;
     let mut entries = Vec::new();
@@ -14,6 +36,27 @@ pub fn ls<P: AsRef<Path>>(path: P) -> anyhow::Result<Vec<PathBuf>> {
     Ok(entries)
 }
 
+/// Recursively lists the contents of the specified path and all its subdirectories.
+///
+/// This function returns a vector of `PathBuf` objects representing all the files and directories
+/// within the specified path and its subdirectories.
+///
+/// # Examples
+///
+/// ```
+/// use durs::ls_rec;
+/// use std::path::Path;
+///
+/// let files = ls_rec(Path::new("/path/to/directory"))?;
+/// for file in files {
+///     println!("{}", file.display());
+/// }
+/// ```
+///
+/// # Errors
+///
+/// This function will return an error if the path cannot be accessed or if there is an error
+/// reading the directory contents.
 pub fn ls_rec<P: AsRef<Path>>(path: P) -> anyhow::Result<Vec<PathBuf>> {
     let meta = path.as_ref().symlink_metadata()?;
     let mut entries = Vec::new();
@@ -32,6 +75,26 @@ pub fn ls_rec<P: AsRef<Path>>(path: P) -> anyhow::Result<Vec<PathBuf>> {
     Ok(entries)
 }
 
+/// Calculates the total size of the specified path and its contents.
+///
+/// If the path is a directory, this function recursively calculates the total size of all files
+/// and subdirectories within the directory.
+/// If the path is a file, this function returns the size of the file.
+///
+/// # Examples
+///
+/// ```
+/// use durs::size;
+/// use std::path::Path;
+///
+/// let total_size = size(Path::new("/path/to/file_or_directory"))?;
+/// println!("Total size: {} bytes", total_size);
+/// ```
+///
+/// # Errors
+///
+/// This function will return an error if the path cannot be accessed or if there is an error
+/// reading the directory contents.
 pub fn size<P: AsRef<Path>>(path: P) -> anyhow::Result<u64> {
     let meta = path.as_ref().symlink_metadata()?;
     let mut bytes = 0;
@@ -150,7 +213,6 @@ mod test {
 
         Ok(())
     }
-
 
     #[test]
     fn test_ls_rec_file() -> anyhow::Result<()> {
